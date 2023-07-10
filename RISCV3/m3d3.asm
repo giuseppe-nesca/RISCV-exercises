be3a93c309a77b766d5afd83d32951984c0b1143
Giuseppe Nesca <giuseppenesca.dev@gmail.com>
2023-07-10 22:54:57 +0200

    fix r3d3

--- RISCV3/m3d3.asm
+++ RISCV3/m3d3.asm
@@ -1,14 +1,11 @@
 .globl _start
 .data
-    num: .dword 8     
+    num: .dword 10        
 .text
 _start:
     # call serief
     la    a0, num
     jal   ra, serief
-    
-    li a7, 1
-    ecall
 
     #exit
     li    a7, 10
@@ -17,49 +14,42 @@ _start:
 #****************************************************
 # completare la funzione serief nel campo di sotto
 
-
-
-serief: # a0: *n 
-	ld t1, (a0)
-	li t0, 3
-	bgt t1, t0, serief_m3
-	li a0, 1
-	ret
+serief: #a0: num
+	li t0, 1 # 1
+	beq a0, t0, serief_ret1
+	addi t0, t0, 1 # 2
+	beq a0, t0, serief_ret1
+	addi t0, t0, 1 # 3
+	beq a0, t0, serief_ret1
 	
-	serief_m3:
-	addi sp, sp, -32
+	addi sp, sp, -24
 	sd ra, 0(sp)
 	sd s1, 8(sp)
 	sd s2, 16(sp)
-	sd s3, 24(sp)
-	
-	mv s1, a0
-	mv s3, t1
 
-	addi t1, s3, -1	
-	sd t1, 0(s1)
+	mv s1, a0
+	
+	addi a0, s1, -1
 	jal serief
 	mv s2, a0
 	
-	addi t1, s3, -2	
-	sd t1, 0(s1)
-	mv a0, s1
+	addi a0, s1, -2
 	jal serief
 	add s2, s2, a0
 	
-	addi t1, s3, -3	
-	sd t1, 0(s1)
-	mv a0, s1
+	addi a0, s1, -3
 	jal serief
 	add a0, s2, a0
 	
-	ld s3, 24(sp)
 	ld s2, 16(sp)
 	ld s1, 8(sp)
 	ld ra, 0(sp)
-	addi sp, sp, 32
+	addi sp, sp, 24
 	ret
 	
+	serief_ret1:
+	li a0, 1
+	ret
 	
 	
 	
